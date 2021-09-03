23ca677e77dcc2425c52493794782de8609389b6
Aymammet <aymammet2018@gmail.com>
2021-09-04 01:34:13 +0500

    s3 settings

--- iface/settings.py
+++ iface/settings.py
@@ -130,6 +130,8 @@ USE_TZ = True
 
 # STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
+MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static' )] 
