27a880997aac156677ed5beb2b7d8328d4abd1d5
Aymammet <aymammet2018@gmail.com>
2021-09-29 23:51:56 +0500

    for heroku deploy2

--- iface/settings/base.py
+++ iface/settings/base.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -81,8 +80,8 @@ WSGI_APPLICATION = 'iface.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': BASE_DIR / 'db,sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -122,9 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/assets/'
 MEDIA_URL = "/media/"
+
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static' )] 
 
@@ -145,5 +144,3 @@ LOGIN_REDIRECT_URL = 'post:home'
 LOGOUT_REDIRECT_URL = 'authenticate:main_page'
 
 SESSION_COOKIE_AGE = 60 * 60 * 24  # 1 day (timedelta)
-
-
