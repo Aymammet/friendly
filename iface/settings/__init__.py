80f880f74075e8688b4c7854d9500aa0011072ff
Aymammet <aymammet2018@gmail.com>
2021-10-06 22:15:41 +0500

    gitignore

--- iface/settings/__init__.py
+++ iface/settings/__init__.py
@@ -8,4 +8,4 @@ from iface.settings.base import *
 environment = os.getenv('DJANGO_SETTINGS_MODULE', 'local')
 
 if environment.endswith('production'):
-    from .production import *
+    from iface.settings.production import *
