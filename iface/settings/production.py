87b48d1f3c84034ecdb0c77783dad98d58883a51
Aymammet <aymammet2018@gmail.com>
2021-10-06 21:27:32 +0500

    added gitignore

--- iface/settings/production.py
+++ iface/settings/production.py
@@ -12,17 +12,20 @@ INSTALLED_APPS += [
     'cloudinary',
     'cloudinary_storage',
 ]
-
+# HTTPS settings
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
-SECURE_HSTS_SECONDS = 15768000  # set low, but when site is ready for deployment, set to at least 15768000 (6 months)
+
 SECURE_SSL_REDIRECT = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 CSRF_COOKIE_SAMESITE = 'Strict'
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 X_FRAME_OPTIONS = 'DENY'
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+
+# HSTS settings
+SECURE_HSTS_SECONDS = 31536000  # 1 year
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True   
 SECURE_HSTS_PRELOAD = True
 
 MIDDLEWARE.append('whitenoise.middleware.WhiteNoiseMiddleware')
